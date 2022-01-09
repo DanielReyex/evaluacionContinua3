34f07ce083c62ed0bd6214b908fb208dfc1d43f4
DanielReyex <paindaniel36@gmail.com>
2022-01-08 23:10:43 -0500

    first commit

--- app/src/main/java/com/example/evaluacioncontinua3/HomeFragment.java
+++ app/src/main/java/com/example/evaluacioncontinua3/HomeFragment.java
@@ -12,7 +12,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+
 public class HomeFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.home_fragment,container,false);
@@ -30,5 +38,10 @@ public class HomeFragment extends Fragment {
         }
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu,MenuInflater menuInflater){
+        menuInflater.inflate(R.menu.toolbar_menu,menu);
+        super.onCreateOptionsMenu(menu,menuInflater);
+    }
 
 }
